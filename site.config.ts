7d62279524c9c9488463bee5684d046d6158f56d
xuwanjin <xuwj@mxchip.com>
2023-06-17 19:59:50 +0800

    修改 rootNotionPageId
    Signed-off-by: xuwanjin <xuwj@mxchip.com>

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'cc1a99ee9f024c459e7779f0626e808d',
+  rootNotionPageId: '28c5a976c6ab454a990f0f03e1016555',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
